9d38dd9ca745c050e1f2a397090bf945aaad5f9d
unknown <7racooma@gmail.com>
2018-12-01 06:30:50 +0900

    잡다한것들

---
+++ Assets/Scripts/RCM/FingerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FingerController : MonoBehaviour {
+
+    public GameObject stone;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        stone = collision.gameObject;
+    }
+
+    public void OnCollisionExit2D(Collision2D collision)
+    {
+        stone = null;
+    }
+}
---
+++ Assets/Scripts/RCM/MouseRoll.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseRoll : MonoBehaviour {
+
+	
+    void FixedUpdate()
+    {
+       
+        Roll();
+    }
+
+    void Roll()
+    {
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)
+        {
+            if (transform.rotation.z < 30  )
+                transform.Rotate(0, 0, 15);
+            
+        }
+
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f)
+        {
+            
+                transform.Rotate(0, 0, -15);
+        }
+    }
+}
