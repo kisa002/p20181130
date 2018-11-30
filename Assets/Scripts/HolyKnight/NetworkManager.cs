5d76273595507e858dab137e92f0d151466be2c4
kisa002 <vnycall74@naver.com>
2018-12-01 04:02:56 +0900

    Network Sync

---
+++ Assets/Scripts/HolyKnight/NetworkManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkManager : MonoBehaviour
+{
+    public static NetworkManager Instance;
+
+    public string version = "0.1";
+
+    public int id = 0;
+
+    private void Awake()
+    {
+        if (NetworkManager.Instance == null)
+            NetworkManager.Instance = this;
+        else
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        ConnectToPhoton();
+    }
+
+    #region Connect
+    public void ConnectToPhoton()
+    {
+        PhotonNetwork.ConnectUsingSettings(version);
+
+        Debug.Log("포톤 접속 중..!");
+    }
+
+    private void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby(TypedLobby.Default);
+
+        Debug.Log("마스터 접속 완료");
+    }
+
+    private void OnJoinedLoby()
+    {
+        Debug.Log("로비 접속 완료");
+    }
+
+    private void OnDisconnectedFromPhoton()
+    {
+        Debug.Log("포톤 접속 해제");
+    }
+    #endregion
+
+    #region Create & Join Room
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions
+        {
+            MaxPlayers = 2
+        };
+
+        PhotonNetwork.CreateRoom("test", roomOptions, null);
+    }
+
+    public void JoinCreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+
+        PhotonNetwork.CreateRoom("test", roomOptions, TypedLobby.Default);
+    }
+
+    public void CreateRoom(string roomName)
+    {
+        PhotonNetwork.CreateRoom(roomName, new RoomOptions() { MaxPlayers = 2 }, null);
+    }
+
+    public void JoinRoom()
+    {
+        PhotonNetwork.JoinRoom("test");
+    }
+
+    public void JoinRoom(string roomName)
+    {
+        PhotonNetwork.JoinRoom(roomName);
+    }
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    private void OnJoinedRoom()
+    {
+        SpawnPlayer();
+        //PhotonNetwork.LoadLevel("InGame");
+        Debug.Log("방 접속 완료");
+    }
+    #endregion
+
+    #region InGame
+    private void OnSceneFinishedLoading(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log($"NAME: {scene.name}");
+        switch (scene.name)
+        {
+            case "InGame":
+                SpawnPlayer();
+                break;
+        }
+    }
+
+    public void SpawnPlayer()
+    {
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);
+    }
+    #endregion
+}
---
+++ Assets/Scripts/HolyKnight/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PhotonView photonView;
+
+    public int id = 0;
+
+    public GameObject[] finger = new GameObject[2];
+    public GameObject[] arm = new GameObject[2];
+
+    void Start ()
+    {
+        if (PhotonNetwork.isMasterClient)
+        {
+            if(photonView.isMine)
+            {
+                id = 1;
+                transform.position = Vector2.left * 7;
+            }
+            else
+            {
+                id = 2;
+                transform.position = Vector2.right * 7;
+
+                transform.rotation = Quaternion.Euler(Vector3.up * 180);
+            }
+        }
+        else
+        {
+            if (photonView.isMine)
+            {
+                id = 2;
+                transform.position = Vector2.right * 7;
+
+                transform.rotation = Quaternion.Euler(Vector3.up * 180);    
+            }
+            else
+            {
+                id = 1;
+                transform.position = Vector2.left * 7;
+            }
+        }
+	}
+	
+	void Update ()
+    {
+        if (!photonView.isMine)
+            return;
+
+        MoveArm();
+    }
+
+    void MoveArm()
+    {
+        var x = Input.GetAxis("Horizontal") * 5 * Time.deltaTime;
+        var y = Input.GetAxis("Vertical") * 17 * Time.deltaTime;
+
+        if (id == 1)
+        {
+            arm[0].transform.parent.Translate(Vector2.right * x);
+            arm[1].transform.Rotate(Vector3.back * y);
+
+            SendArm();
+        }
+        else
+        {
+            arm[0].transform.parent.Translate(Vector2.right * -x);
+            arm[1].transform.Rotate(Vector3.back * -y);
+
+            SendArm();
+        }
+    }
+
+    public void SendFinger(int type)
+    {
+        photonView.RPC("PunSendFinger", PhotonTargets.All, type, finger[type].transform.position, finger[type].transform.rotation);
+    }
+
+    [PunRPC]
+    public void PunSendFinger(int type, Vector3 pos, Quaternion rot)
+    {
+        finger[type].transform.position = pos;
+        finger[type].transform.rotation = rot;
+    }
+
+    public void SendArm()
+    {
+        photonView.RPC("PunSendArm", PhotonTargets.All, arm[0].transform.parent.position, arm[1].transform.rotation);
+    }
+
+    [PunRPC]
+    public void PunSendArm(Vector3 pos, Quaternion rot)
+    {
+        arm[0].transform.parent.position = pos;
+        arm[1].transform.rotation = rot;
+
+        //Debug.Log(pos);
+    }
+}
