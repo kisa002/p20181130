9d29a433fe1abc3724ce90001e40557347dadca4
racooma <7racooma@gmail.com>
2018-12-01 06:34:56 +0900

    ANG

---
+++ Assets/TextController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextController : MonoBehaviour {
+    public Text[] text = new Text[4];
+
+    public List<string> listSentence = new List<string>();
+
+    public string PlayerText;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void No1()
+    {
+       
+        for (int j=0;j<4; j++)
+        {
+        int num = Random.Range(0, 4);
+        bool isCheck = true;
+        while(true)
+        {
+            for (int i = 0; i < 4; i++)
+                if ((text[j].text == text[i].text && j != i) || text[i].text == listSentence[num])
+                         isCheck = false;
+
+            if (isCheck)
+                break;
+        }
+         text[j].text = listSentence[num];
+        }
+        
+        
+    }
+    
+}
---
+++ Assets/TextManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class TextManager : MonoBehaviour {
+    public Text NormalText;
+    
+
+    public void SetText(string text)
+    {
+        NormalText.text = text;
+    }
+
+    public string GetText()
+    {
+        return NormalText.text;
+    }
+
+    
+}
